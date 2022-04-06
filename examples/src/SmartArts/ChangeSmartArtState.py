26c93dd85d67fe166ff14ef2e1749d608d6ca16c
Stanislav Miroshnichenko <stanislav.miroshnichenko@aspose.com>
2022-04-06 17:47:53 +0300

    add SmartArt samples

---
+++ examples/src/SmartArts/ChangeSmartArtState.py
@@ -0,0 +1,19 @@
+﻿import aspose.slides as slides
+
+
+#ExStart:ChangeSmartArtState
+# The path to the documents directory.
+dataDir = "./examples/data/"
+outDir = "./examples/out/"
+
+with slides.Presentation() as presentation:
+    # Add SmartArt BasicProcess 
+    smart = presentation.slides[0].shapes.add_smart_art(10, 10, 400, 300, slides.smartart.SmartArtLayoutType.BASIC_PROCESS)
+
+    # Get or Set the state of SmartArt Diagram
+    smart.is_reversed = True
+    flag = smart.is_reversed
+
+    # Saving Presentation
+    presentation.save(outDir + "smart_art_change_state_out.pptx", slides.export.SaveFormat.PPTX)
+#ExEnd:ChangeSmartArtState
---
+++ examples/src/SmartArts/CustomChildNodesInSmartArt.py
@@ -0,0 +1,37 @@
+﻿import aspose.slides as slides
+
+
+#ExStart:CustomChildNodesInSmartArt
+dataDir = "./examples/data/"
+outDir = "./examples/out/"
+
+# Load the desired the presentation
+with slides.Presentation(dataDir + "smart_art_access.pptx") as pres:
+	smart = pres.slides[0].shapes.add_smart_art(20, 20, 600, 500, slides.smartart.SmartArtLayoutType.ORGANIZATION_CHART)
+
+	# Move SmartArt shape to new position
+	node = smart.all_nodes[1]
+	shape = node.shapes[1]
+	shape.x += (shape.width * 2)
+	shape.y -= (shape.height / 2)
+
+	# Change SmartArt shape's widths
+	node = smart.all_nodes[2]
+	shape = node.shapes[1]
+	shape.width += (shape.width / 2)
+
+	# Change SmartArt shape's height
+	node = smart.all_nodes[3]
+	shape = node.shapes[1]
+	shape.height += (shape.height / 2)
+
+	# Change SmartArt shape's rotation
+	node = smart.all_nodes[4]
+	shape = node.shapes[1]
+	shape.rotation = 90
+
+	pres.save(outDir + "smart_art_custom_child_nodes_out.pptx", slides.export.SaveFormat.PPTX)
+
+#ExEnd:CustomChildNodesInSmartArt
+		
+	
